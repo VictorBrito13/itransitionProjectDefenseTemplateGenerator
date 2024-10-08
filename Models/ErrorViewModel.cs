36f145ba58a57cbaa84de5c46e970b47e822e8cd
VictorBrito13 <javibrito10@gmail.com>
2024-10-07 18:13:52 -0600

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ItransitionTemplates.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
